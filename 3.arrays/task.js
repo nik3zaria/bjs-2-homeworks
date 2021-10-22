2c1133b7e7203f25b36abe66dbf2c8a26a934177
Nikita Cherepanov <nik3zaria@mail.ru>
2021-10-22 10:42:01 +0700

    Praktik-3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,6 @@
 function compareArrays(arr1, arr2) {
   let result;
-
+  result = arr1.length === arr2.length && arr1.every((item, i) => arr2[i] === item);
   // Ваш код
 
   return result; // boolean
@@ -8,7 +8,7 @@ function compareArrays(arr1, arr2) {
 
 function advancedFilter(arr) {
   let resultArr;
-
+  resultArr = arr.filter(item => item > 0 && item % 3 === 0).map(item => item * 10);
   // Ваш код
 
   return resultArr; // array
